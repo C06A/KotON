abbf44babd62d7de1d115d8135ca9103cf8c0a2b
C.A.B <git@helpchoice.com>
2018-02-02 23:59:52 -0500

    Initial commit

---
+++ src/main/kotlin/com/helpchoice/kotlin/koton/KotON.kt
@@ -0,0 +1,112 @@
+package com.helpchoice.kotlin.koton
+
+/**
+ * This was inspired by project https://github.com/Jire/KTON.
+ *
+ * Here I spread content into class hierarchy which helps to scope functions
+ * and doesn't require both Map and Array for each instance.
+ * As a result access to the value requires extra parenthesis.
+ *
+ * Also I added toJson() function to convert the Object into (you guessed it) JSON.
+ */
+open class KotON() {
+    open fun toJson(): String {
+        return "{}"
+    }
+
+    open operator fun get(key: String): KotON {
+        if (this is KotONEntry) {
+            return this[key]
+        }
+        throw Exception("Key access is not supported by this instance")
+    }
+
+    open operator fun get(index: Int): KotON {
+        if (this is KotONArray) {
+            return this[index]
+        }
+        throw Exception("Index is not supported by this instance")
+    }
+
+    open operator fun invoke(): Any? {
+        return null
+    }
+}
+
+data class KotONVal<out T>(val value: T): KotON() {
+    override fun toJson(): String {
+        return when(value) {
+            is String -> "\"$value\""
+            else -> value.toString()
+        }
+    }
+
+    override operator fun invoke(): Any? {
+        return value
+    }
+}
+
+data class KotONArray(val value: ArrayList<KotON> = ArrayList()): KotON() {
+    override fun toJson(): String {
+        return value.map {
+            it.toJson()
+        }.joinToString(prefix = "[", postfix = "]")
+    }
+
+    operator fun plus(body: KotONBuilder.() -> Any): KotONArray {
+        value += kotON(body)
+        return this
+    }
+
+    override operator fun get(index: Int): KotON {
+        return value[index]
+    }
+}
+
+data class KotONEntry(val content: Map<String, KotON> = emptyMap()): KotON() {
+    override fun toJson(): String {
+        return content.entries.joinToString(prefix = "{", postfix = "}") {
+            "\"${it.key}\": ${it.value.toJson()}"
+        }
+    }
+
+    override operator fun get(key: String): KotON {
+        return content[key] ?: super.get(key)
+    }
+}
+
+data class KotONBuilder(val content: MutableMap<String, KotON> = mutableMapOf()) {
+    infix fun <T> String.to(value: T) {
+        content[this] = KotONVal(value)
+    }
+
+    operator fun String.invoke(body: KotONBuilder.() -> Any) {
+        content[this] = kotON(body)
+    }
+
+    operator fun String.get(vararg bodies: KotONBuilder.() -> Unit) {
+        content[this] = kotON(*bodies)
+    }
+
+    fun build(): KotONEntry {
+        return KotONEntry(content)
+    }
+}
+
+inline fun <T> kotON(value: T): KotONVal<T> {
+    return KotONVal(value)
+}
+
+inline fun kotON(init: KotONBuilder.() -> Any): KotONEntry {
+    val root = KotONBuilder()
+    val value = root.init()
+    return root.build()
+}
+
+inline fun kotON(vararg bodies: KotONBuilder.() -> Unit): KotONArray {
+    val kotON = KotONArray()
+    bodies.forEach {
+        kotON + it
+    }
+    return kotON
+}
---
+++ src/test/kotlin/com/helpchoice/kotlin/koton/KotONSpec.kt
@@ -0,0 +1,118 @@
+package com.helpchoice.kotlin.koton
+
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
+
+class KotONSpec: StringSpec() {
+    init {
+        "empty" {
+            KotON().toJson() shouldBe "{}"
+            kotON { }.toJson() shouldBe "{}"
+        }
+
+        "numbers" {
+            kotON(42).toJson() shouldBe "42"
+            kotON(3.14).toJson() shouldBe "3.14"
+        }
+
+        "boolean" {
+            kotON(true).toJson() shouldBe "true"
+            kotON(false).toJson() shouldBe "false"
+        }
+
+        "simple collection" {
+            kotON {
+                "string" to "string value"
+                "integer" to 42
+                "float" to 3.14
+                "boolean true" to true
+                "boolean false" to false
+            }.toJson() shouldBe
+                    "{\"string\": \"string value\", \"integer\": 42, \"float\": 3.14, " +
+                    "\"boolean true\": true, \"boolean false\": false}"
+        }
+
+        "array of simple" {
+            kotON {
+                "array"[
+                        { "stringElement" to "value of an element" },
+                        { "intKey" to 42; "floatKey" to 3.14 },
+                        {
+                            "boolTrue" to true
+                            "booleFalse" to false
+                        }
+                        ]
+            }.toJson() shouldBe
+                    "{\"array\": [{\"stringElement\": \"value of an element\"}, " +
+                    "{\"intKey\": 42, \"floatKey\": 3.14}, " +
+                    "{\"boolTrue\": true, \"booleFalse\": false}]}"
+        }
+
+        "complex structure" {
+            val doc = kotON {
+                "string" to "string value"
+                "integer" to 42
+                "array"[
+                        { "stringElement" to "value of an element" },
+                        { "intKey" to 42; "floatKey" to 3.14 },
+                        {
+                            "boolTrue" to true
+                            "booleFalse" to false
+                        }
+                        ]
+                "float" to 3.14
+                "boolean true" to true
+                "subStruct" {
+                    "substring" to "string value"
+                    "subinteger" to 42
+                    "subfloat" to 3.14
+                    "subarray"[
+                            { "stringElement" to "value of an element" },
+                            { "intKey" to 42; "floatKey" to 3.14 },
+                            {
+                                "boolTrue" to true
+                                "booleFalse" to false
+                            }
+                            ]
+                    "subboolean true" to true
+                    "subboolean false" to false
+                }
+                "boolean false" to false
+            }
+
+            doc["string"]() shouldBe "string value"
+            doc["integer"]() shouldBe 42
+            doc["float"]() shouldBe 3.14
+            doc["boolean true"]() shouldBe true
+            doc["boolean false"]() shouldBe false
+
+            doc["array"][0]["stringElement"]() shouldBe "value of an element"
+            doc["array"][1]["intKey"]() shouldBe 42
+            doc["array"][1]["floatKey"]() shouldBe 3.14
+            doc["array"][2]["boolTrue"]() shouldBe true
+            doc["array"][2]["booleFalse"]() shouldBe false
+
+            val expect = kotON {
+                "expected"[
+                        { "stringElement" to "value of an element" },
+                        { "intKey" to 42; "floatKey" to 3.14 },
+                        {
+                            "boolTrue" to true
+                            "booleFalse" to false
+                        }
+                        ]
+            }
+            doc["subStruct"]["subarray"] shouldBe expect["expected"]
+
+            val expected = kotON(
+                    { "stringElement" to "value of an element" },
+                    { "intKey" to 42; "floatKey" to 3.14 },
+                    {
+                        "boolTrue" to true
+                        "booleFalse" to false
+                    }
+            )
+            doc["subStruct"]["subarray"] shouldBe expected
+        }
+    }
+}
